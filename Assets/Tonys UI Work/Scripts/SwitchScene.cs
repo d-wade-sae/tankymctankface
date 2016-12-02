399dbe4669a28bffed37b9413a0c1d5ba0028490
d-wade-sae <damon1w@optusnet.com.au>
2016-12-02 19:46:16 +1100

    Damons Work
    All of my work and any work i contributed to

---
+++ Assets/Tonys UI Work/Scripts/SwitchScene.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SwitchScene : MonoBehaviour 
+{
+	public string myScene;
+
+	public void PlayScene()
+	{
+		SceneManager.LoadScene (myScene);
+	}
+}
